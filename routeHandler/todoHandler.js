1bf3296a07b1fc0a11899a48e8b08448c8e288a9
AbhijitSarker <abhijitsarker03@gmail.com>
2023-12-24 14:53:13 +0600

    created a todo route

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
 
 
 // GET ALL THE TODOS
@@ -13,9 +16,21 @@ router.get("/:id", async (req, res) => {
 
 });
 
-// POST A TODO
+// Create A TODO
 router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
 
+  await newTodo.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todo was inserted successfully!",
+      });
+    }
+  });
 });
 
 // POST MULTIPLE TODO
