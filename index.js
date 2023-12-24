8fcb3f3ec7d5dfbbe1de4075130b91727973b834
AbhijitSarker <abhijitsarker03@gmail.com>
2023-12-24 14:24:44 +0600

    connected to db

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
+app.use(express.json());
 
+//db connection
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qeddfku.mongodb.net/todos?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true
+})
+    .then(() => console.log('connection established'))
+    .catch(err => console.log(err))
 
 
 const errorHandler = (err, req, res, next) => {
-    if (err.headersSent) {
+    if (res.headersSent) {
         return next(err);
     }
     res.status(500).json({ err: err });
 }
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log(`listening on 3000`)
 })
